adbade66399f58e8f42da2f45f6dacab68589106
gregsim92 <gregsim92@gmail.com>
2016-02-01 11:39:44 -0800

    committing before merge

--- js/controllers.js
+++ js/controllers.js
@@ -2,8 +2,8 @@ app.controller('mainController', function($scope,$http){
 
 	$scope.movie = 'star'
 
-	$http.get('http://www.omdbapi.com/?t=' + $scope.movie + '&plot=short&r=json')
+	$http.get('http://www.omdbapi.com/?s=' + $scope.movie + '&plot=short&r=json')
 		.then(function(response){
-			$scope.data = response.data
+			$scope.data = response.data.Search
 		})
 })
