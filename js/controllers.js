2abacde440fb5ac07f169a9cd7fe723930839a02
Kevin Eddye <kevneddy@gmail.com>
2016-02-01 11:38:30 -0800

    Add landing page search bar/heading

--- js/controllers.js
+++ js/controllers.js
@@ -2,7 +2,7 @@ app.controller('mainController', function($scope,$http){
 
 	$scope.movie = 'star'
 
-	$http.get('http://www.omdbapi.com/?t=' + $scope.movie + '&plot=short&r=json')
+	$http.get('http://www.omdbapi.com/?s=' + $scope.movie + '&plot=short&r=json')
 		.then(function(response){
 			$scope.data = response.data
 		})
